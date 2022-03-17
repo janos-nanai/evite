f8df2b4e0fc9af916076faffb1262a6a2b563076
Janos Nanai <janos.nanai@gmail.com>
2022-03-17 01:57:42 +0100

    login styling improve

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,16 +5,16 @@ import Card from "../components/UI/Card";
 const Auth = () => {
   return (
     <Card title="LOGIN">
-      <form action="submit">
-        <div>
-          <label htmlFor="name">name</label>
-          <input type="text" id="name" />
+      <form className="form" action="submit">
+        <div className="form__input-container">
+          <label className="form__label" htmlFor="name">name</label>
+          <input className="form__input" type="text" id="name" />
         </div>
-        <div>
-          <label htmlFor="password">password</label>
-          <input type="password" id="password" />
+        <div className="form__input-container">
+          <label className="form__label" htmlFor="password">password</label>
+          <input className="form__input" type="password" id="password" />
         </div>
-        <button type="submit" className="btn btn--light">
+        <button className="btn btn--light" type="submit">
           letsgo
         </button>
       </form>
